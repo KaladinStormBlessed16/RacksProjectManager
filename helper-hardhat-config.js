a4c6ba85edbeab706c67f0c93585c53790230cbc
David C.R <davidcr.odalys@gmail.com>
2022-08-15 12:39:07 +0200

    Helper Hardhat Clean up

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,8 +4,6 @@ const networkConfig = {
     },
     4: {
         name: "rinkeby",
-        MRCAddress: "0xef453154766505feb9dbf0a58e6990fd6eb66969",
-        Erc20TokenAddress: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     },
     80001: {
         name: "mumbai",
@@ -14,8 +12,8 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-const frontEndContractsFile = "../NFT-Marketplace-Front/constants/networkMapping.json"
-const frontEndAbiLocation = "../NFT-Marketplace-Front/constants/"
+const frontEndContractsFile = "../"
+const frontEndAbiLocation = "../"
 
 module.exports = {
     networkConfig,
