b4983957bde22d1c0d78ef49dd8ffd9947257131
Daniel Sintimbrean <ddsintim@inf.upv.es>
2022-08-26 16:51:06 +0200

    change an equals to less or equal

--- contracts/RacksProjectManager.sol
+++ contracts/RacksProjectManager.sol
@@ -208,7 +208,7 @@ contract RacksProjectManager is IRacksProjectManager, Ownable, AccessControl {
                 uint256 callerReputationLv = accountToContributorData[msg.sender].reputationLevel;
                 uint256 j = 0;
                 for (uint256 i = 0; i < projects.length; i++) {
-                    if (projects[i].reputationLevel() == callerReputationLv) {
+                    if (projects[i].reputationLevel() <= callerReputationLv) {
                         filteredProjects[j] = projects[i];
                         j++;
                     }
